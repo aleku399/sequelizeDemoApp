5c66cfbe30a2e979a2e1a1b4b9fcbceb2fbe2e24
aleku399 <aleku399@gmail.com>
2018-10-23 11:34:21 +0300

    w.i.p

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
